a3843d0b1d582c8384f0b65b2c2eab72ba33d46d
quintus <john@chaordo.com>
2023-05-30 15:19:17 -0600

    Initial Commit

---
+++ darkMode/darkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  darkModeApp.swift
+//  darkMode
+//
+//  Created by John on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct darkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
